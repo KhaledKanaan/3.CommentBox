6928208574539c7189ac5b126115f72c00fd3373
KhaledKanaan <khaled-kenaan@hotmail.com>
2022-11-25 12:13:04 +0000

    rev.1

---
+++ README.md
@@ -0,0 +1,11 @@
+# 3.CommentBox
+Founders and Coders project no.1: Hobby Page
+
+--------------------------------------------
+rev.0: 
+initial commit
+
+rev.1: 
+1. fix counter reset problem
+2. keep user name and email after posting the comment
+3. change to dynamic comment maximum length
--- script.js
+++ script.js
@@ -4,9 +4,14 @@ var errorMsg = document.getElementById("error-msg");
 
 const maxLength = 140;
 
+// set the counter text referring to the defined maxLength
+counter.innerText = length+`/${maxLength}`;
+
+// add listener listener to the comment box to update the counter and to alert the user when exceeding 140 chars
 commentBox.addEventListener('input', function(event){
 
   var length = commentBox.value.length;
+  // if the comment exceed maxLength the errer is shown and the word counter turns red
   if(length>maxLength){
     counter.style.color = 'red';
     errorMsg.style.visibility = 'visible';
@@ -15,12 +20,13 @@ commentBox.addEventListener('input', function(event){
     errorMsg.style.visibility = 'hidden';
   }
  
-  counter.innerText = length+"/140";
+  // update the counter on each input event
+  counter.innerText = length+`/${maxLength}`;
   
 });
 
 
-
+// on submitting the form
 document.getElementById("form").addEventListener("submit", function (event) {
 
   event.preventDefault();
@@ -30,6 +36,7 @@ document.getElementById("form").addEventListener("submit", function (event) {
   const comment = document.getElementById("comment").value;
   var length = commentBox.value.length;
 
+  // prevent the user from postion if any of the following details is missing or if comment length exceeds maxLength
   if(email==""){
     alert("Please write your email");
   }
@@ -43,6 +50,7 @@ document.getElementById("form").addEventListener("submit", function (event) {
     alert("Your comment exceeds the allowed maximum length and connot be submitted");
   }
 
+  // if everything is fine proceed with posting the comment with user details and date+time
   else{
     var date = new Date();
     var day = date.getDate();
@@ -51,47 +59,50 @@ document.getElementById("form").addEventListener("submit", function (event) {
     var hours = date.getHours();
     var minutes = date.getMinutes();
   
-  
+    // forming the comment time and date to be attached to the comment
     var time = "["+day+"-"+month +"-"+year+"]" +",  "+hours+":"+minutes;
-  
   
     let comments = document.getElementById('comments');
   
-    const newCommentElement = document.createElement("div");
-  
-    //create the time element and and append the time
+    //create the time element and append the time into it
     const timeElement = document.createElement("p");
     const timeNode = document.createTextNode(time);
     timeElement.appendChild(timeNode);
   
-    //create the email element and and append the email
+    //create the email element and append the email into it
     const emailElement = document.createElement("p");
     const emailNode = document.createTextNode(email);
     emailElement.appendChild(emailNode);
   
   
-    //create the name element and and append the name
+    //create the name element and append the name into it
     const nameElement = document.createElement("p");
     const nameNode = document.createTextNode(userName+":");
     nameElement.appendChild(nameNode);
   
-    //create the email element and and append the email
+    //create the comment element and append the comment into it
     const commentElement = document.createElement("p");
     const commentNode = document.createTextNode(comment);
     commentElement.appendChild(commentNode);
   
-    newCommentElement.appendChild(timeElement);
-    newCommentElement.appendChild(emailElement);
-    newCommentElement.appendChild(nameElement);
-    newCommentElement.appendChild(commentElement);
+    // create a new comment package element which will be a package of time + date + email + name + comment, to be added to the comments section
+    const commentPackageElement = document.createElement("div");
+    commentPackageElement.appendChild(timeElement);
+    commentPackageElement.appendChild(emailElement);
+    commentPackageElement.appendChild(nameElement);
+    commentPackageElement.appendChild(commentElement);
   
-    newCommentElement.classList.add("comment");
+    // add the relative styling to the comment package
+    commentPackageElement.classList.add("comment");
   
-    comments.prepend(newCommentElement);
+    //add the comment package to the comments section
+    comments.prepend(commentPackageElement);
 
-    document.getElementById("email").value="";
-    document.getElementById("name").value="";
+    // clear all the comment text input for the next comment 
     document.getElementById("comment").value="";
+
+    //clear the counter
+    counter.innerText = `0/${maxLength}`;
   }
 });
      
